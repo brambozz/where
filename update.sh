4e3bf921fac179f9a58b6c3836b9f0a22c98e0a9
brambozz <brambo5@live.nl>
2024-03-04 12:26:29 +0100

    Update update script to properly merge events

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-cat ~/.local/share/khal/calendars/private/*.ics > where.ics
+echo "BEGIN:VCALENDAR" >> merge;
+for file in ~/.local/share/khal/calendars/private/*.ics; do 
+cat "$file" | sed -e '$d' $1 | sed -e '1,/VEVENT/{/VEVENT/p;d;}' $2  >> merge; 
+done
+mv merge where.ics
+echo "END:VCALENDAR" >> where.ics;
+
 git add where.ics
 git commit -m 'update where.ics'
 git push
