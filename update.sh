d5acc57bb3525a9c392a354cf8753fd65fd19fb6
Bram de Wilde <brambo5@live.nl>
2024-04-03 16:14:18 +0200

    Change update to new google calendar workflow

--- update.sh
+++ update.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-echo "BEGIN:VCALENDAR" >> merge;
-for file in ~/.local/share/khal/calendars/private/*.ics; do 
-cat "$file" | sed -e '$d' $1 | sed -e '1,/VEVENT/{/VEVENT/p;d;}' $2  >> merge; 
-done
-mv merge where.ics
-echo "END:VCALENDAR" >> where.ics;
+rm where.ics
+unzip *.zip
+mv *.ics where.ics
+rm *.zip
 
 git add where.ics
 git commit -m 'update where.ics'
